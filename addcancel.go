d1c1982eea18696265a86110a67227955ab6c9c6
zikaeroh <48577114+zikaeroh@users.noreply.github.com>
2024-05-04 21:18:20 -0700

    Simplify using context.AfterFunc

--- addcancel.go
+++ addcancel.go
@@ -2,8 +2,6 @@ package ctxjoin
 
 import (
 	"context"
-	"sync"
-	"sync/atomic"
 	"time"
 )
 
@@ -14,31 +12,33 @@ func init() {
 }
 
 type cancelContext struct {
-	main       context.Context
-	cancelMain context.CancelFunc
-	extra      context.Context
-
-	doneOnce sync.Once
-	done     chan struct{}
-
-	err atomic.Value
+	ctx   context.Context
+	extra context.Context
 }
+
+var _ context.Context = (*cancelContext)(nil)
 
 // AddCancel creates a context with the same values and cancellation as the main context,
 // but which will additionally be canceled when the extra context is canceled.
 func AddCancel(main context.Context, extra context.Context) (context.Context, context.CancelFunc) {
-	main, cancel := context.WithCancel(main)
-	j := &cancelContext{
-		main:       main,
-		cancelMain: cancel,
-		extra:      extra,
+	// This is similar to the "Merge" example for context.WithCancelCause, handling deadlines.
+	ctx, cancelWithCause := context.WithCancelCause(main)
+	stop := context.AfterFunc(extra, func() { cancelWithCause(context.Cause(extra)) })
+	cancel := func() {
+		stop()
+		cancelWithCause(context.Canceled)
 	}
 
-	return j, j.cancel
+	j := &cancelContext{
+		ctx:   ctx,
+		extra: extra,
+	}
+
+	return j, cancel
 }
 
 func (c *cancelContext) Deadline() (deadline time.Time, ok bool) {
-	deadline, ok = c.main.Deadline()
+	deadline, ok = c.ctx.Deadline()
 	if !ok {
 		return c.extra.Deadline()
 	}
@@ -52,44 +52,13 @@ func (c *cancelContext) Deadline() (deadline time.Time, ok bool) {
 }
 
 func (c *cancelContext) Done() <-chan struct{} {
-	// Lazily start the goroutine when this channel is requested.
-	// We can create the done chan from the parent contexts at any time.
-	c.doneOnce.Do(c.runCloser)
-	return c.done
-}
-
-func (c *cancelContext) cancel() {
-	c.doneOnce.Do(func() {
-		c.done = closedChan
-		c.err.Store(context.Canceled)
-	})
-	c.cancelMain()
-}
-
-func (c *cancelContext) runCloser() {
-	c.done = make(chan struct{})
-	go func() {
-		defer close(c.done)
-
-		var err error
-		select {
-		case <-c.main.Done():
-			err = c.main.Err()
-		case <-c.extra.Done():
-			err = c.extra.Err()
-		}
-
-		c.err.Store(err)
-	}()
+	return c.ctx.Done()
 }
 
 func (c *cancelContext) Err() error {
-	if err := c.err.Load(); err != nil {
-		return err.(error)
-	}
-	return nil
+	return c.ctx.Err()
 }
 
 func (c *cancelContext) Value(key interface{}) interface{} {
-	return c.main.Value(key)
+	return c.ctx.Value(key)
 }
